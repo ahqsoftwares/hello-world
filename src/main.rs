666d3de96bbac86f91eee9469f00108dfbc4bd97
AHQ Miness <ahqsecret@gmail.com>
2023-02-27 20:27:18 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use crypter;
+
 fn main() {
-    let data = include!("./encryption");
-    println!("{data}");
+    let pass = include!("./encryption");
+    let payload = "mega ultra safe payload";
+
+    let encrypted = crypter::encrypt(pass.as_bytes(), payload.as_bytes()).expect("Failed to encrypt");
+
+    println!("{:?}", &encrypted);
+    let decrypted = crypter::decrypt(pass.as_bytes(), &encrypted).expect("Failed to decrypt");
+    println!("{}", String::from_utf8(decrypted).expect("Invalid decrypted string"));
 }
